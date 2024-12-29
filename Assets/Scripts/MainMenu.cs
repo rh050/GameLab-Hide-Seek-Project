f3be184898e428fb9822e23825e528222d8a13df
daniel tzur <danielchacha99@gmail.com>
2024-12-29 14:28:00 +0200

    portal + night mode updated
    added portal and night mode

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StartGame();
+        }
+    }
+    
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+        
     }
 
     public void QuitGame()
