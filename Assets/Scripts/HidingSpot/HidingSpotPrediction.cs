0f2166b14daed63e96d9b006197fd6e3b3d6b515
Dolest <151120420+Dolest@users.noreply.github.com>
2024-12-30 19:10:22 +0200

    Update HidingSpotPrediction.cs
    update2

--- Assets/Scripts/HidingSpot/HidingSpotPrediction.cs
+++ Assets/Scripts/HidingSpot/HidingSpotPrediction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HidingSpotPrediction : MonoBehaviour
 {
+    int i;
     public HidingSpot GetPredictedSpot()
     {  
         return HidingSpotManager.Instance.GetRandomAvailableSpot();
