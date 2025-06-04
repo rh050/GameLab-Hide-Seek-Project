92187b5fccc14a404e4425ae7d46dd42caf73c4f
Dolest <151120420+Dolest@users.noreply.github.com>
2025-06-04 17:46:59 +0300

    game menu + keyboard panel

---
+++ Assets/Scripts/UI/GameMenu/PauseManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        bool isActive = pausePanel.activeSelf;
+        pausePanel.SetActive(!isActive);
+        Time.timeScale = isActive ? 1 : 0;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu"); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
